d2d6db0e5cc93f44fa16aaa0ae2e8e845c0c8d5c
Benjamin Chambers <benjamindchambers@outlook.com>
2015-08-05 17:15:26 -0600

    Changed the display on the Customization page to be more intuitive (toggle switch On shows that segment, Off hides it)

---
+++ Porter/Pages/Main/Customization/BoolNegator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Porter.Pages.Main.Customization
+{
+    public class BoolNegator : IValueConverter
+    {
+        public Object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool)
+                return !((bool)value);
+            return null;
+        }
+
+        public Object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool)
+                return !((bool)value);
+            return null;
+        }
+    }
+}
