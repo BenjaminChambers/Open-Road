3a4b74fd47409258e6eb671219c71eb415328912
Benjamin Chambers <benjamindchambers@outlook.com>
2015-08-06 09:19:56 -0600

    Cleaned up some navigation code in the pages

--- Porter/Pages/FillupList/EditFillup/EditFillupPage.xaml.cs
+++ Porter/Pages/FillupList/EditFillup/EditFillupPage.xaml.cs
@@ -29,10 +29,8 @@ namespace Porter.Pages.FillupList.EditFillup
 
         public EditFillupPage()
         {
-            this.InitializeComponent();
-            this.navigationHelper = new NavigationHelper(this);
-            this.navigationHelper.LoadState += this.NavigationHelper_LoadState;
-            this.navigationHelper.SaveState += this.NavigationHelper_SaveState;
+            InitializeComponent();
+            InitializeNavigation();
 
             using (var db = Util.Database.Connection())
             {
@@ -66,26 +64,21 @@ namespace Porter.Pages.FillupList.EditFillup
             FillupForm.DataContext = FormData;
         }
 
+
+        // Navigation stuff
+        private void InitializeNavigation()
+        {
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += this.NavigationHelper_LoadState;
+            this.navigationHelper.SaveState += this.NavigationHelper_SaveState;
+        }
         private NavigationHelper navigationHelper;
         private ObservableDictionary defaultViewModel = new ObservableDictionary();
-        public NavigationHelper NavigationHelper {get { return this.navigationHelper; }}
-        public ObservableDictionary DefaultViewModel
-        {
-            get { return this.defaultViewModel; }
-        }
-        private void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
-        {
-        }
-        private void NavigationHelper_SaveState(object sender, SaveStateEventArgs e)
-        {
-        }
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            this.navigationHelper.OnNavigatedTo(e);
-        }
-        protected override void OnNavigatedFrom(NavigationEventArgs e)
-        {
-            this.navigationHelper.OnNavigatedFrom(e);
-        }
+        public NavigationHelper NavigationHelper { get { return this.navigationHelper; } }
+        public ObservableDictionary DefaultViewModel { get { return this.defaultViewModel; } }
+        private void NavigationHelper_LoadState(object sender, LoadStateEventArgs e) { }
+        private void NavigationHelper_SaveState(object sender, SaveStateEventArgs e) { }
+        protected override void OnNavigatedTo(NavigationEventArgs e) { this.navigationHelper.OnNavigatedTo(e); }
+        protected override void OnNavigatedFrom(NavigationEventArgs e) { this.navigationHelper.OnNavigatedFrom(e); }
     }
 }
